bd6eea55f769e12e294526ce0153c0d75e6bf161
Kirill Hrudzinski <604karev@gmail.com>
2024-04-21 10:40:54 +0200

    fix root path to browserrouter basename

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "pages/HomePage";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
       </Routes>
